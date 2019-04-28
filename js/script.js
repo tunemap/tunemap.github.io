d12d378725a1acef97999a828d8092a4ee069fc7
Archmonger <>
2019-04-28 15:50:11 -0700

    remove mockup from production

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const node = document.getElementById('searchbox');
-node.addEventListener("keyup", function(event) {
-	if (event.key === "Enter") {
-		window.location.replace("./html/album-cloud.html");
-	}
-});
