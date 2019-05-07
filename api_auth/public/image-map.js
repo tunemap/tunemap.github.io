99880db041dfc332d59271e20802841d5a6c4ea9
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-05-07 14:15:29 -0700

    setup for justifiedjs (broken)

--- api_auth/public/image-map.js
+++ api_auth/public/image-map.js
@@ -1,3 +1,24 @@
+var photos = [];
+$(window).load(function() {
+	$('.layout-1').empty().justifiedImages({
+		images: photos,
+		thumbnailPath: function(photo, width, height) {
+			var purl = photo.url_s;
+			if (photo.url_n && (width > photo.width_s * 1.2 || height > photo.height_s * 1.2)) purl = photo.url_n;
+			if (photo.url_m && (width > photo.width_n * 1.2 || height > photo.height_n * 1.2)) purl = photo.url_m;
+			if (photo.url_z && (width > photo.width_m * 1.2 || height > photo.height_m * 1.2)) purl = photo.url_z;
+			if (photo.url_l && (width > photo.width_z * 1.2 || height > photo.height_z * 1.2)) purl = photo.url_l;
+			return purl;
+		},
+		getSize: function(photo) {
+			return {
+				width: photo.width_s,
+				height: photo.height_s
+			};
+		},
+	});
+});
+
 (function() {
 
 	/**
@@ -52,7 +73,6 @@
 				},
 				success: function(response) {
 					userTopTenPlaceholder.innerHTML = userTopTenTemplate(response.items);
-
 					$('#login').hide();
 					$('#loggedin').show();
 				}
