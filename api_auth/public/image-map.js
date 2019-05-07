bb2c93da233e280eee0f632c5ad7ddc29545a787
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-05-07 15:43:00 -0700

    finalize justified gallery layout

--- api_auth/public/image-map.js
+++ api_auth/public/image-map.js
@@ -1,24 +1,3 @@
-var photos = [];
-$(window).load(function() {
-	$('.layout-1').empty().justifiedImages({
-		images: photos,
-		thumbnailPath: function(photo, width, height) {
-			var purl = photo.url_s;
-			if (photo.url_n && (width > photo.width_s * 1.2 || height > photo.height_s * 1.2)) purl = photo.url_n;
-			if (photo.url_m && (width > photo.width_n * 1.2 || height > photo.height_n * 1.2)) purl = photo.url_m;
-			if (photo.url_z && (width > photo.width_m * 1.2 || height > photo.height_m * 1.2)) purl = photo.url_z;
-			if (photo.url_l && (width > photo.width_z * 1.2 || height > photo.height_z * 1.2)) purl = photo.url_l;
-			return purl;
-		},
-		getSize: function(photo) {
-			return {
-				width: photo.width_s,
-				height: photo.height_s
-			};
-		},
-	});
-});
-
 (function() {
 
 	/**
@@ -123,3 +102,10 @@ $(window).load(function() {
 		}, false);
 	}
 })();
+
+$(window).load(function() {
+	$("#layout-1").justifiedGallery({
+		rowHeight: document.documentElement.clientHeight / 3.5,
+		margins: 5
+	});
+});
